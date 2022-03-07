94ab8e522e986370626b1cfc511cfee713e82c90
quentin-pierson <quentin62.pierson@yahoo.com>
2022-03-07 14:54:32 +0000

    reformat file google-spanner for get the old scrapper

--- google-spanner.py
+++ google-spanner.py
@@ -1,8 +1,9 @@
 from google.cloud import spanner
 from scrapper import Scrapper
 from goodScrapper import GoodScrapper
-from datetime import datetime
+from datetime import datetime, date
 import uuid
+
 
 
 class Google_Spanner:
@@ -33,43 +34,43 @@ class Google_Spanner:
         with database.snapshot() as snapshot:
             return snapshot.execute_sql(f"SELECT MAX(id) +1 from {table_name}")
 
-    def insert_account(self, database, shared_data):
+    def insert_account(self, database, account):
         with database.batch() as batch:
             batch.insert(
                 table="Account",
                 columns=("id", "account_id", "username", "nb_followers", "nb_following", "nb_medias", "is_verified", 
                 "is_private"),
                 values=[
-                    (uuid.uuid4(), int(shared_data['id']), shared_data['username'], 
-                    shared_data['edge_followed_by']['count'], shared_data['edge_follow']['count'], 2, 
-                    shared_data['is_verified'], shared_data['is_private'])
+                    (str(uuid.uuid4()), int(account.identifier), account.username, account.follows_count, account.followed_by_count, account.media_count, 
+                     account.is_verified, account.is_private)
                 ],
             )
         print("Inserted data on Account")
 
     def insert_media(self, database, media):
-        date_comment = media.created_at
-        print("je suis passer chez sohs")
+        #datetime.fromtimestamp(date_comment)
         with database.batch() as batch:
+            date_comment = media.created_time
             batch.insert(
                 table="Media",
                 columns=("id", "account_id", "tags", "id_media", "date", "nb_comments", "nb_likes", "link", "caption", "type"),
                 values=[
-                    (str(uuid.uuid4()), int(media.owner.identifier), ["1"], int(media.identifier), datetime.fromtimestamp(date_comment), 
+                    (str(uuid.uuid4()), int(media.owner.identifier), ["1"], int(media.identifier), date.today() , 
                     media.comments_count, media.likes_count, media.link, media.caption, media.type)
                 ],
             )
         print("Inserted data on Media")
-    
+
+
     def insert_comments(self, database, comment):
-        date_comment = comment.created_at
+        #date_comment = comment.created_time
         with database.batch() as batch:
             batch.insert(
                 table="Comments",
                 columns=("id", "id_media", "username", "date", "content"),
                 values=[
-                    (str(uuid.uuid4()), int(comment.owner.identifier), comment.owner.username, 
-                    datetime.fromtimestamp(date_comment), comment.text)
+                    (str(uuid.uuid4()), str(comment.owner.identifier), comment.owner.username, 
+                    date.today(), comment.text)
                 ],
             )
         print("Inserted data on Comments")
@@ -89,20 +90,30 @@ class Google_Spanner:
     def run(self):
         print("coucou")
         googleSpanner = self.connect()
-        self.insert_account(googleSpanner, account)        
+        self.insert_account(googleSpanner, scrapperInformation)        
+        for media in medias:
+            self.insert_media(googleSpanner, media)
+
+        for comment in comments['comments']:
+            self.insert_comments(googleSpanner, comment)
+
+         # TO DO 
+         # Tags 
+         #
+
+        self.test_request(googleSpanner, "Account")
+
+
+scrapper = Scrapper("Nabille_CompteFan", "mK5RnE84i")
+insta = scrapper.connect(20)
+scrapperInformation = scrapper.get_account_information(insta, "billieeilish")
+medias = scrapper.get_medias(insta, 594081073, 5)
+comments = scrapper.get_comments(insta, 2784465257312196115, 3)
 
 
 
-args = {"login_user": "Nabille_CompteFan", "login_pass": "mK5RnE84i"}
-scrapper = GoodScrapper(args)
-connexion = scrapper.connect(True)
-account = scrapper.get_account_information(connexion, "kanyewest")
 
-
-
-
-
-GS = Google_Spanner("tfgen-spanid-20220303195152835", "database-insta-scrapping")
+GS = Google_Spanner("tfgen-spanid-20220307112400987", "database-insta-scrapping")
 GS.run()
 
 
